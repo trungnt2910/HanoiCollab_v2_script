4cfee40d29935cc5a147695c606ab95299d5b3bd
Trung <57174311+trungnt2910@users.noreply.github.com>
2022-03-12 21:37:29 +0700

    fix: Support for Google Forms
    HanoiCollab now works correctly on Google Form's multiple choice
    group data, as well as on forms with multiple pages.

--- src/Forms/GoogleDocsFormProvider.ts
+++ src/Forms/GoogleDocsFormProvider.ts
@@ -70,7 +70,7 @@ class GoogleDocsFormProvider extends FormProvider
     {
         return new Promise<boolean>((resolve, reject) =>
         {
-            if (!window.location.href.includes("/viewform"))
+            if (!(window.location.href.includes("/viewform") || window.location.href.includes("/formResponse")))
             {
                 resolve(false);
                 return;
@@ -119,6 +119,57 @@ class GoogleDocsFormProvider extends FormProvider
         }).observe(inputCollection, {subtree: true, childList: true, attributes: true, attributeOldValue : true, attributeFilter: ["value"]})
     }
 
+    SetupCommunityAnswersUI(): void 
+    {
+        for (let q of HanoiCollabGlobals.Questions)
+        {
+            // Standalone questions.
+            if (q.HtmlElement.getAttribute("role") == "listitem")
+            {
+                q.HtmlElement.appendChild(q.CommunityAnswersHtml);
+            }
+            else
+            {
+                // Limited space, so we'll provide a hover menu.
+                q.CommunityAnswersHtml.style.display = "none";
+                q.CommunityAnswersHtml.style.position = "fixed";
+                q.CommunityAnswersHtml.style.top = "-69420px";
+                q.CommunityAnswersHtml.style.margin = "0";
+                q.CommunityAnswersHtml.style.padding = "0";
+                q.CommunityAnswersHtml.style.background = "rgba(0,127,255,0.9)";
+                q.CommunityAnswersHtml.style.zIndex = "9997";
+                q.CommunityAnswersHtml.style.minWidth = "10%";
+                q.HtmlElement.appendChild(q.CommunityAnswersHtml);
+                q.HtmlElement.addEventListener("mouseenter", function(ev)
+                {
+                    if (!HanoiCollabGlobals.IsStealthMode)
+                    {
+                        q.CommunityAnswersHtml.style.display = "block";
+                        q.CommunityAnswersHtml.style.left = `${ev.screenX}`;
+                        q.CommunityAnswersHtml.style.top = `${ev.screenY}`;    
+                    }
+                });
+                q.HtmlElement.addEventListener("mousemove", function(ev)
+                {
+                    if (!HanoiCollabGlobals.IsStealthMode)
+                    {
+                        q.CommunityAnswersHtml.style.display = "block";
+                        q.CommunityAnswersHtml.style.left = `${ev.screenX}`;
+                        q.CommunityAnswersHtml.style.top = `${ev.screenY}`;    
+                    }
+                });
+                q.HtmlElement.addEventListener("mouseleave", function()
+                {
+                    if (!HanoiCollabGlobals.IsStealthMode)
+                    {                
+                        q.CommunityAnswersHtml.style.display = "none";
+                        q.CommunityAnswersHtml.style.top = "-69420px"; 
+                    }
+                });
+            }
+        }
+    }
+
     GetType(): FormProviderType
     {
         return FormProviderType.GoogleDocs;
@@ -133,7 +184,7 @@ class GoogleDocsFormProvider extends FormProvider
 
     GetFormId(): string 
     {
-        return "" + window.location.href.match(`https:\/\/docs\.google\.com\/forms\/d\/e\/(.*?)\/viewform.*`)![1];
+        return "" + (window.location.href.match(`https:\/\/docs\.google\.com\/forms\/d\/e\/(.*?)\/viewform.*`) ?? window.location.href.match(`https:\/\/docs\.google\.com\/forms\/d\/e\/(.*?)\/formResponse.*`))![1];
     }
 
     ExtractResources(): string[] 
@@ -156,12 +207,15 @@ class GoogleDocsFormProvider extends FormProvider
     ExtractQuestions(): QuestionLayout[] 
     {
         var result = [];
-        var questions = HanoiCollabGlobals.Window.FB_PUBLIC_LOAD_DATA_[1][1];
         var questionElements = document.querySelectorAll("[role=\"listitem\"]");
-        for (let i = 0; i < questions.length; ++i)
+        for (let i = 0; i < questionElements.length; ++i)
         {
-            var q = questions[i];
             var qElem = questionElements[i];
+            var q = JSON.parse((qElem.querySelector("[data-params]") as HTMLElement)?.dataset?.params?.replace("%.@.", "[") ?? "null")?.at(0);
+            if (q == null)
+            {
+                continue;
+            }
             var currentQuestionType = QuestionType.None;
             switch (q[3])
             {
@@ -171,7 +225,10 @@ class GoogleDocsFormProvider extends FormProvider
                 case 1:
                     currentQuestionType = QuestionType.Written;
                 break;
+                // Multiple choice
                 case 2:
+                // Multiple choice _GROUP_
+                case 7:
                     currentQuestionType = QuestionType.MultipleChoice;
                 break;
             }
@@ -179,39 +236,79 @@ class GoogleDocsFormProvider extends FormProvider
             {
                 continue;
             }
-            var currentQuestionDescription = q[1];
-            var currentQuestionId = "" + q[4][0][0];
-            var currentQuestionAnswers = [];
-            var resourcesInAnswers = new Array<string>();
-            if (currentQuestionType == QuestionType.MultipleChoice)
-            {
-                var answers = q[4][0][1];
-                var answerElements = qElem.querySelectorAll("label");
-                var alpha = "A";
-                for (let j = 0; j < answers.length; ++j)
-                {
-                    var a = answers[j];
-                    var aElem = answerElements[j];
-                    var imageResources = Array.from(aElem.querySelectorAll("img")).map(i => i.src);
-                    var resources = Array.from(aElem.querySelectorAll("iframe")).map(i => i.src);
-                    resourcesInAnswers.push(...imageResources);
-                    resourcesInAnswers.push(...resources);
-                    currentQuestionAnswers.push(new AnswerLayout(a[0], resources, a[0].getHashCode(), String.fromCharCode(alpha.charCodeAt(0) + j), imageResources));
-                }
-            }
-            else if (currentQuestionType == QuestionType.Hybrid)
-            {
-                var alpha = "A";
-                for (let j = 0; j < 4; ++j)
-                {
-                    currentQuestionAnswers.push(new AnswerLayout("", [], String.fromCharCode(alpha.charCodeAt(0) + j), String.fromCharCode(alpha.charCodeAt(0) + j), []));
-                }
-            }
-            
-            var currentQuestionImageResources = Array.from(qElem.querySelectorAll("img")).map(i => i.src).filter(i => !resourcesInAnswers.includes(i));
-            var currentQuestionResources = Array.from(qElem.querySelectorAll("iframe")).map(i => i.src).filter(i => !resourcesInAnswers.includes(i));
 
-            result.push(new QuestionLayout(currentQuestionType, currentQuestionDescription, currentQuestionId, currentQuestionAnswers, currentQuestionResources, currentQuestionImageResources));
+            // Questions with subquestions
+            if (q[3] == 7)
+            {
+                var currentMainQuestionDescription = q[1];
+                var currentMainQuestionImageResources = Array.from(qElem.querySelectorAll("img")).map((i: HTMLImageElement) => i.src);
+                var currentMainQuestionResources = Array.from(qElem.querySelectorAll("iframe")).map((i: HTMLIFrameElement) => i.src);
+
+                var subquestions = q[4];
+                var subquestionElements = qElem.querySelectorAll("[role=\"presentation\"]");
+
+                for (let j = 0; j < subquestions.length; ++j)
+                {
+                    var subquestionId = "" + subquestions[j][0];
+                    var subquestionAnswers = subquestions[j][1];
+                    var subquestionDescription = subquestions[j][3][0];
+                    var currentQuestionDescription = currentMainQuestionDescription + " -- " + subquestionDescription;
+
+                    // Subquestions cannot have resources.
+                    var currentQuestionImageResources = currentMainQuestionImageResources;
+                    var currentQuestionResources = currentMainQuestionResources;
+                    var currentQuestionId = subquestionId;
+                    var currentQuestionAnswers = [];
+
+                    var answers = subquestionAnswers;
+                    var alpha = "A";
+                    for (let j = 0; j < answers.length; ++j)
+                    {
+                        var a = answers[j];
+                        currentQuestionAnswers.push(new AnswerLayout(a[0], [], a[0].getHashCode(), String.fromCharCode(alpha.charCodeAt(0) + j), []));
+                    }
+                    
+                    result.push(new QuestionLayout(currentQuestionType, currentQuestionDescription, currentQuestionId, currentQuestionAnswers, currentQuestionResources, currentQuestionImageResources));
+                }
+            }
+            // Questions without subquestions
+            else
+            {
+                var currentQuestionDescription = q[1] as string;
+                var currentQuestionId = "" + q[4][0][0];
+                var currentQuestionAnswers = [];
+                var resourcesInAnswers = new Array<string>();
+                if (currentQuestionType == QuestionType.MultipleChoice)
+                {
+                    var answers = q[4][0][1];
+                    var answerElements = qElem.querySelectorAll("label");
+                    var alpha = "A";
+                    for (let j = 0; j < answers.length; ++j)
+                    {
+                        var a = answers[j];
+                        var aElem = answerElements[j];
+                        var imageResources = Array.from(aElem.querySelectorAll("img")).map(i => i.src);
+                        var resources = Array.from(aElem.querySelectorAll("iframe")).map(i => i.src);
+                        resourcesInAnswers.push(...imageResources);
+                        resourcesInAnswers.push(...resources);
+                        currentQuestionAnswers.push(new AnswerLayout(a[0], resources, a[0].getHashCode(), String.fromCharCode(alpha.charCodeAt(0) + j), imageResources));
+                    }
+                }
+                else if (currentQuestionType == QuestionType.Hybrid)
+                {
+                    var alpha = "A";
+                    for (let j = 0; j < 4; ++j)
+                    {
+                        currentQuestionAnswers.push(new AnswerLayout("", [], String.fromCharCode(alpha.charCodeAt(0) + j), String.fromCharCode(alpha.charCodeAt(0) + j), []));
+                    }
+                }
+                
+                var currentQuestionImageResources = Array.from(qElem.querySelectorAll("img")).map(i => i.src).filter(i => !resourcesInAnswers.includes(i));
+                var currentQuestionResources = Array.from(qElem.querySelectorAll("iframe")).map(i => i.src).filter(i => !resourcesInAnswers.includes(i));
+    
+                result.push(new QuestionLayout(currentQuestionType, currentQuestionDescription, currentQuestionId, currentQuestionAnswers, currentQuestionResources, currentQuestionImageResources));
+            }
+
         }
         return result;    
     }
@@ -219,12 +316,15 @@ class GoogleDocsFormProvider extends FormProvider
     GetQuestionInfos(): QuestionInfo[] 
     {
         var result = [];
-        var questions = HanoiCollabGlobals.Window.FB_PUBLIC_LOAD_DATA_[1][1];
         var questionElements = document.querySelectorAll("[role=\"listitem\"]");
-        for (let i = 0; i < questions.length; ++i)
+        for (let i = 0; i < questionElements.length; ++i)
         {
-            var q = questions[i];
             var qElem = questionElements[i] as HTMLElement;
+            var q = JSON.parse((qElem.querySelector("[data-params]") as HTMLElement)?.dataset?.params?.replace("%.@.", "[") ?? "null")?.at(0);
+            if (q == null)
+            {
+                continue;
+            }
             var currentQuestionType = QuestionType.None;
             switch (q[3])
             {
@@ -235,6 +335,7 @@ class GoogleDocsFormProvider extends FormProvider
                     currentQuestionType = QuestionType.Written;
                 break;
                 case 2:
+                case 7:
                     currentQuestionType = QuestionType.MultipleChoice;
                 break;
             }
@@ -242,92 +343,102 @@ class GoogleDocsFormProvider extends FormProvider
             {
                 continue;
             }
-            var currentQuestionId = "" + q[4][0][0];
-            var info = new QuestionInfo(qElem, currentQuestionId, currentQuestionType, i);
-            if (currentQuestionType == "multipleChoice")
+            var subElems = qElem.querySelectorAll("[role=\"presentation\"]");
+            for (var j = 0; j < q[4].length; ++j)
             {
-                var answers = q[4][0][1];
-                var alpha = "A";
-                for (let j = 0; j < answers.length; ++j)
+                var currentQuestionId = "" + q[4][j][0];
+                var currentElem = qElem;
+                // Question group
+                if (q[3] == 7)
                 {
-                    var a = answers[j];
-                    info.Answers.push({Id: "" + a[0].getHashCode(), Alpha: String.fromCharCode(alpha.charCodeAt(0) + j)});
+                    currentElem = subElems[j] as HTMLElement;
                 }
-            }
-            else if (currentQuestionType == "hybrid")
-            {
-                var alpha = "A";
-                for (let j = 0; j < 4; ++j)
+                var info = new QuestionInfo(currentElem, currentQuestionId, currentQuestionType, i);
+                if (currentQuestionType == "multipleChoice")
                 {
-                    info.Answers.push({Id: String.fromCharCode(alpha.charCodeAt(0) + j), Alpha: String.fromCharCode(alpha.charCodeAt(0) + j)});
+                    var answers = q[4][0][1];
+                    var alpha = "A";
+                    for (let j = 0; j < answers.length; ++j)
+                    {
+                        var a = answers[j];
+                        info.Answers.push({Id: "" + a[0].getHashCode(), Alpha: String.fromCharCode(alpha.charCodeAt(0) + j)});
+                    }
                 }
-            }
-
-            info.GetUserAnswer = function()
-            {
-                var info = this;
-                var input = HanoiCollabGlobals.Document.querySelector("[name=\"entry." + info.Id + "\"]") as HTMLInputElement;
-                var content = input?.value;
-                if (!content || content.length == 0)
+                else if (currentQuestionType == "hybrid")
                 {
-                    return "";
+                    var alpha = "A";
+                    for (let j = 0; j < 4; ++j)
+                    {
+                        info.Answers.push({Id: String.fromCharCode(alpha.charCodeAt(0) + j), Alpha: String.fromCharCode(alpha.charCodeAt(0) + j)});
+                    }
                 }
-                if (info.Type == "multipleChoice")
+    
+                info.GetUserAnswer = function()
                 {
-                    return content.getHashCode();
-                }
-                else
-                {
-                    return content;
-                }
-            }
-
-            info.SetUserAnswer = function(answer)
-            {
-                if (this.SendUserAnswer)
-                {
-                    this.SendUserAnswer(answer);
-                }
-            }
-
-            info.ClearUserAnswer = function()
-            {
-                var info = this;
-                var input = HanoiCollabGlobals.Document.querySelector("[name=\"entry." + info.Id + "\"]") as HTMLInputElement;
-                // Clear from DOM
-                if (input)
-                {
-                    input.value = "";   
+                    var info = this;
+                    var input = HanoiCollabGlobals.Document.querySelector("[name=\"entry." + info.Id + "\"]") as HTMLInputElement;
+                    var content = input?.value;
+                    if (!content || content.length == 0)
+                    {
+                        return "";
+                    }
                     if (info.Type == "multipleChoice")
                     {
-                        input.remove();
+                        return content.getHashCode();
                     }
-                }
-
-                // Clear from UI
-                if (info.IsWritten())
-                {
-                    var writeArea = info.HtmlElement.querySelector("input,textarea") as HTMLInputElement | HTMLTextAreaElement;
-                    if (writeArea)
+                    else
                     {
-                        writeArea.value = "";
-                        writeArea.dispatchEvent(new InputEvent("input", {bubbles: true}));
+                        return content;
                     }
                 }
-                else
+    
+                info.SetUserAnswer = function(answer)
                 {
-                    var elem = info.HtmlElement.querySelector("[aria-checked='true']");
-                    if (elem)
+                    if (this.SendUserAnswer)
                     {
-                        elem.setAttribute("aria-checked", "false");
-                        elem.setAttribute("tabindex", "-1");
-                        var clazz = elem.classList[elem.classList.length - 1];
-                        elem.classList.remove(clazz);    
+                        this.SendUserAnswer(answer);
                     }
                 }
+    
+                info.ClearUserAnswer = function()
+                {
+                    var info = this;
+                    var input = HanoiCollabGlobals.Document.querySelector("[name=\"entry." + info.Id + "\"]") as HTMLInputElement;
+                    // Clear from DOM
+                    if (input)
+                    {
+                        input.value = "";   
+                        if (info.Type == "multipleChoice")
+                        {
+                            input.remove();
+                        }
+                    }
+    
+                    // Clear from UI
+                    if (info.IsWritten())
+                    {
+                        var writeArea = info.HtmlElement.querySelector("input,textarea") as HTMLInputElement | HTMLTextAreaElement;
+                        if (writeArea)
+                        {
+                            writeArea.value = "";
+                            writeArea.dispatchEvent(new InputEvent("input", {bubbles: true}));
+                        }
+                    }
+                    else
+                    {
+                        var elem = info.HtmlElement.querySelector("[aria-checked='true']");
+                        if (elem)
+                        {
+                            elem.setAttribute("aria-checked", "false");
+                            elem.setAttribute("tabindex", "-1");
+                            var clazz = elem.classList[elem.classList.length - 1];
+                            elem.classList.remove(clazz);    
+                        }
+                    }
+                }
+    
+                result.push(info);
             }
-
-            result.push(info);
         }
         return result;            
     }
